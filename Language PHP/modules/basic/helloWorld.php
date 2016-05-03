9ae1dda71f1bc4ceae742c2f083b1383713595b6
Maikon Canuto <maikoncanuto@gmail.com>
2016-05-03 14:24:24 -0300

    Update
    Add files php

---
+++ Language PHP/modules/basic/helloWorld.php
@@ -0,0 +1,22 @@
+<?php
+    
+    //Basic PHP: Introduction
+
+    /**
+     * @copyright (c) 2016, Maikon Canuto  MK 
+     */
+
+    //Function print  PHP
+    print("Hello world using print");
+    
+    //Function echo  PHP
+    echo "<hr>"; //puts line
+    
+    //Function echo  PHP
+    echo("Hello World using echo");
+    
+    //Function echo  PHP
+    echo "<hr>"; //puts line
+
+?>
+
