40bdbbbe11bee0eef6a3e0e3cc4fd7c3ba646d0f
Maikon Canuto <maikoncanuto@gmail.com>
2016-05-24 23:33:04 -0300

    Update
    - Adicionando funções e arrumando bugs.

--- BACK-END/Language C/BrincandoComArquivos.c
+++ BACK-END/Language C/BrincandoComArquivos.c
@@ -1,15 +1,30 @@
 //Fazendo testes com gravação de arquivo em C 
-//Em desenvolvimento !! 
+//Em desenvolvimento
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define BUFF 1024
+#define NOME 50
+#define CPF 11
+#define SENHA 5
+#define VALOR 10
 
-void registra_informacao(const char *string, const char *tipo, const char *info){
+typedef struct cliente{
+    char nome[NOME],
+         cpf[CPF],
+         senha[SENHA],
+         valor[VALOR];
+}Cliente;
+
+Cliente *cria_pessoa(){
+    return (Cliente*)malloc(sizeof(Cliente)*1);
+}
+
+void registra_informacao(const char *caminho, const char *categoria, const char *info){
     char aux[BUFF] = "";
-    sprintf(aux, "%s-%s.txt", string, tipo);
+    sprintf(aux, "%s-%s.txt", caminho, categoria);
 
     FILE *arq = fopen(aux, "w+");
 
@@ -17,23 +32,23 @@ void registra_informacao(const char *string, const char *tipo, const char *info)
         fprintf(arq, "%s", info);
         fclose(arq);
     }
+
+    fclose(arq);
 }
 
-char *busca_informacao(const char *string, const char *tipo){
-    char aux[BUFF] = "", tmp[BUFF] = "", letra;
-    sprintf(aux, "%s-%s.txt", string, tipo);
+char *busca_informacao(const char caminho[], const char categoria[]){
+    char aux[BUFF] = "", tmp[BUFF] = "", letra[BUFF] = "";
+    sprintf(aux, "%s-%s.txt", caminho, categoria);
 
     FILE *arq = fopen(aux, "r+");
 
     if(arq != NULL){
-        while(!feof(arq)){
-            letra = fgetc(arq);
-            sprintf(tmp, "%c", letra);
-        }
+        while(fscanf(arq, "%s", letra) != EOF)
+
         fclose(arq);
     }
-
-    return tmp;
+    fclose(arq);
+    return letra;
 }
 
 int comparaStrings(const char *string, const char *str){
@@ -61,12 +76,62 @@ void mostraVetor(const char *string){
     }
 }
 
+int login(){
+    Cliente *pessoa = cria_pessoa();
+    char *cpf, *senha;
+
+    system("cls");
+
+    fflush(stdin);
+    printf("\nDigite seu CPF: ");
+    gets(pessoa->cpf);
+
+    cpf = busca_informacao(pessoa->cpf, "cpf");
+
+    fflush(stdin);
+    printf("\nDigite sua senha: ");
+    gets(pessoa->senha);
+
+    senha = busca_informacao(pessoa->senha, "senha");
+
+    if((comparaStrings(pessoa->senha, senha) == 1 ) && (comparaStrings(pessoa->cpf, cpf) == 1))
+        return 1;
+    else
+        return 0;
+}
+
+void cadastra_cliente(){
+    Cliente *pessoa = cria_pessoa();
+
+    fflush(stdin);
+    printf("\nDigite seu CPF: ");
+    gets(pessoa->cpf);
+    registra_informacao(pessoa->cpf, "cpf", pessoa->cpf);
+
+    fflush(stdin);
+    printf("\nDigite seu nome: ");
+    gets(pessoa->nome);
+    registra_informacao(pessoa->cpf, "nome", pessoa->nome);
+
+    fflush(stdin);
+    printf("\nDigite sua senha: ");
+    gets(pessoa->senha);
+    registra_informacao(pessoa->cpf, "senha", pessoa->senha);
+
+    fflush(stdin);
+    printf("\nDigite um valor inicial: ");
+    gets(pessoa->valor);
+    registra_informacao(pessoa->cpf, "valor", pessoa->valor);
+}
+
 int main(){
-    char *nome = busca_informacao("026", "cpf");
+    //cadastra_cliente();
 
-    registra_informacao("026", "cpf", "Maikon"); //funciona
-    printf("\nBuscar Informacao: %s", nome);
-    printf("\nStrings: %d\n", comparaStrings("Maikon", "Maikon"));
-
+    if(login()){
+        printf("\nLogado com sucesso");
+    }else{
+        printf("\nSENHA E CPF incorretos");
+    }
 return 0;
 }
+
