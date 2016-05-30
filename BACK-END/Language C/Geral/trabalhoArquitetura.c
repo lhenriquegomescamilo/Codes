c7837d2ee3a1adbf2f3860bd7905fa8b949762ef
Maikon Canuto <maikoncanuto@gmail.com>
2016-05-30 16:09:54 -0300

    Update
    Comentando

--- BACK-END/Language C/Geral/trabalhoArquitetura.c
+++ BACK-END/Language C/Geral/trabalhoArquitetura.c
@@ -1,6 +1,7 @@
 //Em desenvolvimento
 
-#include <stdio.h>
+//Importando bibliotecas 
+#include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
  
@@ -48,7 +49,7 @@ int alocaMemoria(const unsigned int mem){
         
         //Determina tamanho da memoria alocada, de acordo com as possibilidades em BITS. Valor aproximado ao digitado
         for(i = 0; i < mem; i++){
-                resultado = elev(i, 2);
+                resultado = elev(i, 2); 
  
                 if(resultado <= mem)
                         aux = resultado;
@@ -85,37 +86,41 @@ int acharLinhas(const unsigned int mem, const unsigned int bloco){
 
 void hexaParaBinario(const unsigned int hexa, const unsigned int endereco){
     unsigned int i, resultado = hexa, binario[endereco];
-   
+    
+    //Determina valor em bainario e salva no vetor binario
+    //Utilizando divisao sucessiva 
     for(i = 0; resultado != 0; i++){
         binario[i] = resultado % 2;
         resultado -= binario[i]/2;
     }
-   
+    
+    //Informa o valor da memoria em hexa 
     printf("\n\t\tENDERECO MP CONSIDERADO: %x =  ", hexa);
- 
+    
+    //Apresenta o endereco em binario 
     for(i = (endereco - 1) ; i >= 0; i--)
         printf("%d", binario[i]);
  
     printf("\n\t\t");
 }
  
-Chama funcao principal
+//Chama funcao principal
 int main(){
-       unsigned  int mp = 0,
-             mc = 0,
-             alocaMemoriaPrincipal = 0,
-                     alocaMemoriaCache = 0,
-                     bloco = 0,
-                     palavra = 0,
-                     endereco = 0,
-                     linhas = 0,
-                     op = 0,
-                     conjunto = 0,
-                     rotulo = 0,
-                 hexa = 0;
+       unsigned  int mp = 0, //Memoria principal 
+                     mc = 0, //Memoria Cache 
+                     alocaMemoriaPrincipal = 0, //Recebe valor alocado da memoria principal 
+                     alocaMemoriaCache = 0, //Recebe valor alocado da memoria cache
+                     bloco = 0, //Recebe valor do bloco 
+                     palavra = 0, //Recebe valor da palavra 
+                     endereco = 0, //Recebe o valor do endereco 
+                     linhas = 0, //Recebe valor da linha 
+                     op = 0, //Opcao 
+                     conjunto = 0, //Valor do conjunto 
+                     rotulo = 0, //Valor do rotulo 
+                     hexa = 0; //Valor em hexadecimal digitado pelo usuario. 
    
-   
-        do{
+    //Menu para interecao com usuario 
+     do{
         system("clear");
  
         printf("\n 1 - Mapeamento Direto");
@@ -123,51 +128,57 @@ int main(){
         printf("\n 3 - Mapeamento Associativo");
         printf("\n 4 - Sair");
         printf("\n\nDigite sua opcao: ");
-        scanf("%u", &op);
+        scanf("%u", &op); //%u por que recebe um "unsigned integer" 
  
         switch(op){
             case 1:
                 system("clear");
- 
+                
+                //Recebe tamanho da memoria principal 
                 printf("\nTAMANHO DA MEMORIA PRINCIPAL: ");
-                scanf("%u", &mp);
-               
+                scanf("%u", &mp); //%u por que recebe um "unsigned integer" 
+                
+                //verifica se é maior que 0, não pode ser negativa 
                 if(mp >= 0){
-                    alocaMemoriaPrincipal = alocaMemoria(mp);
-                    endereco = acharIndice(alocaMemoriaPrincipal, MIBYTES);
-                    printf("\n-----> TAMANHO DA MP ALOCADO: %d MiBytes", alocaMemoriaPrincipal);
-                    printf("\n-----> ENDERECO: %d Bits\n", endereco);
+                    alocaMemoriaPrincipal = alocaMemoria(mp); //Retorna o valor mais proximo ao digitado pelo usuario 
+                    endereco = acharIndice(alocaMemoriaPrincipal, MIBYTES); //Determina endereco da memoria principal 
+                    printf("\n-----> TAMANHO DA MP ALOCADO: %d MiBytes", alocaMemoriaPrincipal); //Mostra tamanho alocado 
+                    printf("\n-----> ENDERECO: %d Bits\n", endereco); //Mostra endereco em bits 
                 }else{
-                    printf("\n-----> ERRO AO ALOCAR MEMORIA PRINCIPAL \n");
+                    printf("\n-----> ERRO AO ALOCAR MEMORIA PRINCIPAL \n"); //Caso seja memoria negativa informa erro 
                 }
- 
+                
+                //Verifica tamanho da memoria cache 
                 printf("\nTAMANHO DA MEMORIA CACHE: ");
-                scanf("%u", &mc);
-               
+                scanf("%u", &mc); //%u por que recebe um "unsigned integer" 
+                
+                //Verifica se é negativo 
                 if(mc >= 0){
-                    alocaMemoriaCache = alocaMemoria(mc);
-                    printf("\n-----> TAMANHO DA MC ALOCADO: %d KiBytes", alocaMemoriaCache);
+                    alocaMemoriaCache = alocaMemoria(mc); //Aloca memoria cache 
+                    printf("\n-----> TAMANHO DA MC ALOCADO: %d KiBytes", alocaMemoriaCache); //Tamaanho da memoria cache 
                 }else{
-                    printf("\n-----> ERRO AO ALOCAR MEMORIA CACHE\n\n");
+                    printf("\n-----> ERRO AO ALOCAR MEMORIA CACHE\n\n"); //Caso tenha alguma erro 
                 }
- 
+                
+                //Tamanho do bloco digitado pelo usuario 
                 printf("\n\nTAMANHO DO BLOCO: ");
-                scanf("%u", &bloco);
- 
+                scanf("%u", &bloco); //%u por que recebe um "unsigned integer" 
+                
+                //Bloco acima de 0 entao: 
                 if(bloco > 0){
-                    palavra = acharIndice(bloco, 1);
-                    printf("\n-----> TAMANHO DO BLOCO: %d Bytes", bloco);
-                    printf("\n-----> TAMANHO DA PALAVRA: %d Bits\n", palavra);
+                    palavra = acharIndice(bloco, 1); //Determina tamanho da palavra 
+                    printf("\n-----> TAMANHO DO BLOCO: %d Bytes", bloco); //Mostra tamanho do bloco 
+                    printf("\n-----> TAMANHO DA PALAVRA: %d Bits\n", palavra); //Mostra palavra em bits 
                 }else{
-                    printf("\n-----> ERRO AO DETERMINAR O BLOCO\n");
+                    printf("\n-----> ERRO AO DETERMINAR O BLOCO\n"); //Mostra erro 
                 }
  
-                linhas = alocaMemoriaCache/bloco;
-                conjunto = acharIndice(linhas, KIBYTES);
-                rotulo = endereco - (conjunto + palavra);
+                linhas = alocaMemoriaCache/bloco; //Faz calculos para achar as linhas 
+                conjunto = acharIndice(linhas, KIBYTES); //Determina conjuntos 
+                rotulo = endereco - (conjunto + palavra); //Determina rotulo 
  
- 
-                printf("\nDeterminando linhas...\n");
+                //Mostra informacoes 
+                printf("\nDeterminando linhas...\n"); 
                 printf("\n-----> %d KiLinhas", linhas);
                 printf("\n-----> %d Bits Conjunto\n\n", conjunto);
                
@@ -184,9 +195,10 @@ int main(){
                 printf("\n\t\tENDERECO DO CONJUNTO: %d Bits\n", conjunto);
                 printf("\n\t\tMAPEAMENTO DO CACHE: DIRETO\n");
                
-               
+                
+                //Pega hexa digitado pelo usuario e apresenta em binario 
                 printf("\n\t\tDIGITE ENDERECO HEXA[%u/HEXA]: ", ((endereco/4) + 1));
-                scanf("%x", &hexa);
+                scanf("%x", &hexa); //Pega em hexadecimal %x 
                
                 hexaParaBinario(hexa, endereco);
                
