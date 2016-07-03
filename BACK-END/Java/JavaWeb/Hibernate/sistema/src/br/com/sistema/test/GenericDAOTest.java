9e3197d182828d184fd7f2783a51f41c4dc1fbd1
Maikon Canuto <maikoncanuto@gmail.com>
2016-07-03 12:24:51 -0300

    Update

--- BACK-END/Java/JavaWeb/Hibernate/sistema/src/br/com/sistema/test/GenericDAOTest.java
+++ BACK-END/Java/JavaWeb/Hibernate/sistema/src/br/com/sistema/test/GenericDAOTest.java
@@ -9,7 +9,10 @@ import br.com.sistema.generico.GenericDAO;
 
 public class GenericDAOTest {
 	
+	
+	
 	@Test
+	@Ignore
 	public void saveTest(){
 		GenericDAO<Fabricante, Long> dao = new GenericDAO<>(Fabricante.class);
 		dao.save(new Fabricante("EMPRESA GOOGLE"));
@@ -20,7 +23,46 @@ public class GenericDAOTest {
 	
 	@Test
 	@Ignore
+	public void updateTest(){
+		GenericDAO<Fabricante, Long> dao = new GenericDAO<>(Fabricante.class);
+		Fabricante fabricante = new Fabricante();
+		fabricante.setCodigo(4L);
+		fabricante.setDescricao("GOOGLE");
+		
+		dao.update(fabricante);
+		
+		new GenericDAO<>(Fabricante.class).findAll().stream().forEach(f -> System.out.println(f));
+	}
+	
+	@Test
+	@Ignore
+	public void searchTest(){
+		GenericDAO<Fabricante, Long> dao = new GenericDAO<>(Fabricante.class);
+		System.out.println(dao.search(4L));
+	}
+	
+	@Test
+	@Ignore
 	public void finAllTest(){
 		new GenericDAO<>(Funcionario.class).findAll().stream().forEach(f -> System.out.println(f));
+		new GenericDAO<>(Fabricante.class).findAll().stream().forEach(f -> System.out.println(f));
+	}
+	
+	@Test
+	@Ignore
+	public void deleteIdentifier(){
+		GenericDAO<Fabricante, Long> dao = new GenericDAO<>(Fabricante.class);
+		dao.delete(4L);
+		
+		new GenericDAO<>(Fabricante.class).findAll().stream().forEach(f -> System.out.println(f));
+	}
+	
+	@Test
+	@Ignore
+	public void deleteObject(){
+		GenericDAO<Fabricante, Long> dao = new GenericDAO<>(Fabricante.class);
+		dao.delete(new Fabricante(28L, "EMPRESA CENTRAL T.I"));
+		
+		new GenericDAO<>(Fabricante.class).findAll().stream().forEach(f -> System.out.println(f));
 	}
 }
